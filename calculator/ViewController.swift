6c2e7cc762f7d80ab536ad2440dcd3db43aee5ed
rytkmt <rytkmt@gmail.com>
2019-01-13 11:51:50 +0900

    initial commit

---
+++ calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by ryo on 2019/01/13.
+//  Copyright © 2019年 rytkmt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
